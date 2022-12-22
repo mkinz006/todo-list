d63ac9084724cd3cd226c5cdc8b6c49254c42e7e
mkinz006 <116930684+mkinz006@users.noreply.github.com>
2022-12-22 11:44:18 -0700

    Shifted Item display to own component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TodoItem } from '../todo-item';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss']
 })
+
 export class ItemComponent {
+  @Input() todoItem: TodoItem = {dateCreated : new Date(),isCompleted:false,itemDescription:''}
 
 }
